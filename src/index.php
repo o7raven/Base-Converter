ef26741e2d75cf44cde0e3d6480bdf62a2adb5c6
yakubiq raven <yakubiq@proton.me>
2023-10-15 23:10:55 +0200

    cleared code

--- src/index.php
+++ src/index.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="styles.css">
     <title>Převodník</title>
 </head>
+</head>
 <body>
     <div class="wrapper">
         <div class="header">
@@ -15,71 +16,63 @@
         </div>
         <div class="form__container">
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="GET">
-                <div class="grid__container">
-                    <div class="valNum">
-                        <span class="brackets"></span>
-                        <input type="text" id="num" name="num" value=
-                        <?php 
+                <div class="form__container__grid">
+                    <label for="num">Číslo</label>
+                    <input type="text" id="num" name="num" value=
+                    <?php 
                         if(isset($_GET['num'])){
                             echo ($_GET['num']);
                         }
                         else{
                             echo "128";
                         }
-                        ?> 
-                         maxlength="9" size="1" required="required">
-                        <span class="brackets"></span>               
-                        <sub>
-                            <input type="text" class="numSys" name="numSys" required="required" value=<?php 
-                        if(isset($_GET['numSys'])){
-                            echo ($_GET['numSys']);
-                        }
-                        else{
-                            echo "10";
-                        }
-                        ?> 
-                        maxlength="2" size="2">
-                        </sub>
-                    </div>
-                    <div class="toNumSys">
-                        <label for="toSys">do soustavy </label>
-                        <input type="text" id="toSys" name="toSys" required="required" value=
-                        <?php 
-                        if(isset($_GET['toSys'])){
-                            echo ($_GET['toSys']);
-                        }
-                        else{
-                            echo "2";
-                        }
-                        ?> 
-                        maxlength="3" size="9">
-                    </div>
-                </div>
-                <input type="submit" class="submit_button" name="submit" value="Převést">
-            </form>
-            <div class="baseValue">
-                <?php
-                if(isset($_GET['submit'])){
-                    $num = $_GET['num'];
-                    $numSys = $_GET['numSys'];
-                    $toSys = $_GET['toSys'];
-                    if($numSys>36 || $toSys>36){
-                        echo "<span style='color:red;''> Error < data-type=float overflow ></span>";
-                    }else{
-                        echo "<h2 style='color:lime;'>(".calculateNumber($num, $numSys, $toSys).")<sub>".$toSys."</sub></h2>";
+                    ?> maxlength="9" size="1" required="required">
+                    <label for="numBase">Ze soustavy</label>
+                    <input type="text" id="numBase" name="numBase" required="required" value=
+                    <?php
+                    if(isset($_GET['numBase'])){
+                        echo ($_GET['numBase']);
                     }
-                }       
-                ?>
+                    else{
+                        echo "10";
+                    }
+                    ?> maxlength="2" size="2">
+                    <label for="toBase">Do soustavy</label>
+                    <input type="text" id="toBase" name="toBase" required="required" value=
+                    <?php
+                    if(isset($_GET['toBase'])){
+                        echo ($_GET['toBase']);
+                    }
+                    else{
+                        echo "2";
+                    }
+                    ?>
+                    >
+                    <input type="submit" id="submit_button" name="submit" value="Převod">
+                </div>
+            </form>
+            <div class="conversionResult">
+            <?php
+            if(isset($_GET['submit'])){
+                $num = $_GET['num'];
+                $numSys = $_GET['numBase'];
+                $toSys = $_GET['toBase'];
+                if($numSys>36 || $toSys>36){
+                    echo "<span style='color:red;''> Error < data-type=float overflow ></span>";
+                }else{
+                    echo "<h2 style='color:lime;'>(".$num.")<sub>".$numSys."</sub> = (".calculateNumber($num, $numSys, $toSys).")<sub>".$toSys."</sub></h2>";
+                }
+            }       
+            ?>
             </div>
         </div>
-
+        <div class="footer">
+            <span class="link"><a href="https://www.pojfm.cz" target="_blank"><b>POJ</b></a></span>
+            <span><script type="text/javascript">document.write( new Date().getFullYear() );</script></span>
+            <span>Jakub Farník</span>
+        </div>
     </div>
-    <div class="footer">
-        <span class="link"><a href="https://www.pojfm.cz" target="_blank"><b>POJ</b></a></span>
-        <span><script type="text/javascript">document.write( new Date().getFullYear() );</script></span>
-        <span>Jakub Farník</span>
-    </div>
-<?php
+    <?php
 
 function toDecimal($number, $numberSystem){
     return base_convert($number, $numberSystem, 10);
